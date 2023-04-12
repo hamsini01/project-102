007ca707fb8025898f35768276bc790c975eef14
hamsini01 <117655138+hamsini01@users.noreply.github.com>
2023-04-12 21:04:44 +0530

    Add files via upload

---
+++ organise.py
@@ -0,0 +1,23 @@
+import os
+import shutil
+from_dir="C:/Users/Hamsini/Downloads"
+to_dir="C:/Users/Hamsini/project102"
+list_of_files = os.listdir(from_dir)
+for file_name in  list_of_files :
+    name,extension = os.path.splitext(file_name)
+    if(extension == ""):
+        continue
+    if(extension in [".txt",".doc",".docx",".pdf"]):
+        path1= from_dir + "/" + file_name 
+        path2 = to_dir + "/"+"image_file"
+        
+        path3 = to_dir + "/"+ "image_file"+file_name
+        if os.path.exists(path2):
+            print("moving " + file_name + ".....")
+            shutil.move(path1,path3)
+        else :
+            os.makedirs(path2)
+            print("moving " + file_name + ".....")
+            shutil.move(path1,path3)
+        
+
